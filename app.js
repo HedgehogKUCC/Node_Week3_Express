3096d836f9b70606780bc4a31e54a065403c5e8c
HedgehogKU <ccc908925@gmail.com>
2022-04-24 17:28:09 +0800

    安裝 cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -9,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 require('./connections/mongoDB');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
