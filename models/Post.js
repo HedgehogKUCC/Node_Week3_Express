8cebe23f81fe0fdedf28a4572c16dcc51463aab9
HedgehogKU <ccc908925@gmail.com>
2022-04-30 19:51:59 +0800

    修正 Date.now() 造成產生時間都一樣的問題

--- models/Post.js
+++ models/Post.js
@@ -20,7 +20,7 @@ const postSchema = new mongoose.Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now(),
+            default: Date.now,
         }
     },
     {
